e37e7309b80f95fe0a2a76e137b5b9e8c31ea5bd
Ross Fenning <ross.fenning@bbc.co.uk>
2021-06-30 00:37:35 +0100

    Intial

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+NAME = "hypertk"
+setup(
+    name=NAME,
+    use_scm_version={
+        "local_scheme": "dirty-tag",
+        "write_to": f"{NAME}/_version.py",
+        "fallback_version": "0.0.0",
+    },
+    author="Ross Fenning",
+    author_email="github@rossfenning.co.uk",
+    packages=[NAME],
+    package_data={NAME: ["py.typed"]},
+    description="{{cookiecutter.description}}",
+    setup_requires=[
+        "setuptools_scm>=3.3.1",
+        "pre-commit",
+    ],
+    install_requires=[],
+    extras_require={
+        "test": ["pytest", "hypothesis", "pytest-pikachu", "pytest-mypy"],
+    },
+)
